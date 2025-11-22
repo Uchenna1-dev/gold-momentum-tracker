774693843c3376dcf2696e86ffd2d31c4810fd68
Uchenna <maoforma50@gmail.com>
2025-11-22 22:08:56 +0000

    updates to compute indicators

--- src/compute_indicators.py
+++ src/compute_indicators.py
@@ -1,19 +1,25 @@
 def compute_indicators(df):
     # calculate indicators
 
+
     #daily_percentage_change():
+        daily_percentage_change = ((today_close - yesterday_close) / yesterday_close) * 100
         
+        today_close = 
+        yesterday_close = 
 
     #sma_5():
-        
+     #simple moving average of last 5 closes   
 
     #ema_21():
-        
+    #exponential moving average of the last 21 closes    
 
     #ema_vs_sma():
         
 
     #volatility():
+    volatility = candle_high - candle_low
+#is this calculating volatility using each candle high and low OR just the overall daily low and high
         
 
     #momentum_signal():
